45a6d98c1c4061538bb99c2e6d447737e0a288da
Kuntola Ray <kuray21@gmail.com>
2016-11-15 23:42:07 -0500

    commit 11/15/2016

--- index.php
+++ index.php
@@ -17,11 +17,25 @@
 
 <?php 
 	print_r($_REQUEST);
+
+	echo "<br />";
+	echo "<br />";
+
+	if ($_REQUEST["username"] == "kakon" && $_REQUEST["password"] == "pass") {
+
+		$userForSession = $_REQUEST["username"];
+		session_start();
+		$_SESSION["username"] = $userForSession;
+		$_SESSION["login_status"] = true;
+		
+		echo "Username is: " . $_SESSION["username"] . ".<br>";
+		echo "Login Status: " . $_SESSION["login_status"] . ".<br>";
+	}
 ?>
 
 <div class="login-page">
 	<div class="form">
-	    <form class="register-form" method="post" action="">
+	    <form class="register-form" method="post" action="login.php">
 	        <input type="text" placeholder="name" name="name"/>
 	        <input type="password" placeholder="password" name="password"/>
 	        <input type="text" placeholder="email address" name="email"/>
---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+	session_start();
+	echo $_SESSION['username'];
+	echo "<br />";
+	print_r($_SESSION)
+?>
